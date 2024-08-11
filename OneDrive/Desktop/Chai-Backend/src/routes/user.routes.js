1ede4272d435fd7774adcb82b11cb09a060fd43f
sidhantrajput1 <sidhant808310@gmail.com>
2024-08-11 10:39:16 +0530

    add new routes for user controller

--- OneDrive/Desktop/Chai-Backend/src/routes/user.routes.js
+++ OneDrive/Desktop/Chai-Backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js"
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannleProfile, getwatchHistory } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middlewares.js"
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
 
@@ -26,5 +26,12 @@ router.route('/login').post(loginUser)
 
 router.route('/logout').post(verifyJwt ,logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJwt, changeCurrentPassword)
+router.route('/current-user').get(verifyJwt, getCurrentUser)
+router.route('/update-account').patch(verifyJwt, updateAccountDetails)
+router.route('/avatar').patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route('/cover-imgage').patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
+router.route('/c/:username').get(verifyJwt, getUserChannleProfile)
+router.route('/watch-history').get(verifyJwt, getwatchHistory)
 
 export default router
